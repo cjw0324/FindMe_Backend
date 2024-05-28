d4f9c75087a5a877dbe4c1ba48146706e4241850
choijaewoo <cchoi00332244@gmail.com>
2024-05-28 11:21:10 +0900

    pagenation function erase

--- src/main/java/com/findme/FindMeBack/Controller/SearchController/SearchFilterController.java
+++ src/main/java/com/findme/FindMeBack/Controller/SearchController/SearchFilterController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,7 +18,7 @@ public class SearchFilterController {
 
     private final SearchFilterService searchFilterService;
 
-    @GetMapping("/search/{page}")
+    @GetMapping("/search/")
     public PaginatedSearchResultDto searchWithLocationCode(
             @RequestParam String nfdlctcd,
             @RequestParam String placekeyword,
@@ -25,19 +26,33 @@ public class SearchFilterController {
             @RequestParam String foundPlace,
             @RequestParam String ymd,
             @RequestParam String placeEtc,
-            @RequestParam String itemEtc,
-            @PathVariable int page
+            @RequestParam String itemEtc
     ) throws ParseException {
         System.out.println(nfdlctcd+","+placekeyword+","+productCategory+","+foundPlace+","+ymd+","+placeEtc+", "+itemEtc);
-        int pageSize = 10; // 페이지당 아이템 수
-        int offset = (page - 1) * pageSize; // 오프셋 계산
+////        int pageSize = 10; // 페이지당 아이템 수
+////        int offset = (page - 1) * pageSize; // 오프셋 계산
+//
+//        List<InfoItem> items =  searchFilterService.searchFilter(nfdlctcd, productCategory, foundPlace, ymd, placekeyword.trim(), placeEtc, itemEtc); //place 앞, 뒤 공백 제거(trim)
+//        Long totalResultCounts = (long) items.size();
+//        List<InfoItem> paginatedItems = new ArrayList<>();
+////        = items.subList(offset, Math.min(offset + pageSize, items.size())); // 페이지네이션
+//        // InfoItem을 InfoDto로 변환
+//        List<SearchDto> searchDtos = paginatedItems.stream().map(item -> new SearchDto(
+//                item.getAtcId(),
+//                item.getFdPrdtNm(),
+//                item.getDepPlace(),
+//                item.getFdFilePathImg(),
+//                item.getFdYmd(),
+//                item.getFdPlace()
+//        )).toList();
+//
+//        return new PaginatedSearchResultDto(totalResultCounts, searchDtos);
 
-        List<InfoItem> items =  searchFilterService.searchFilter(nfdlctcd, productCategory, foundPlace, ymd, placekeyword.trim(), placeEtc, itemEtc); //place 앞, 뒤 공백 제거(trim)
+        List<InfoItem> items = searchFilterService.searchFilter(nfdlctcd, productCategory, foundPlace, ymd, placekeyword.trim(), placeEtc, itemEtc);
         Long totalResultCounts = (long) items.size();
-        List<InfoItem> paginatedItems = items.subList(offset, Math.min(offset + pageSize, items.size())); // 페이지네이션
 
         // InfoItem을 InfoDto로 변환
-        List<SearchDto> searchDtos = paginatedItems.stream().map(item -> new SearchDto(
+        List<SearchDto> searchDtos = items.stream().map(item -> new SearchDto(
                 item.getAtcId(),
                 item.getFdPrdtNm(),
                 item.getDepPlace(),
